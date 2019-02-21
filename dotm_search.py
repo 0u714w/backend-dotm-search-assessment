291b56a29ff7b5764f8845f89a812b9464d9e898
Doug Enas <douglassenas@gmail.com>
2019-02-21 18:38:53 -0500

    added print statements

--- dotm_search.py
+++ dotm_search.py
@@ -6,7 +6,6 @@ Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
 import os
-import sys
 import zipfile
 import argparse
 import glob
@@ -16,6 +15,7 @@ __author__ = "dougenas"
 
 def search_text(character, path):
     """Searches a directory for .dotm files, then searches that file for a specific character"""
+    print('Searching directory {} for text {}').format(path, character)
     files_searched = 0
     files_matched = 0
     os.chdir(path)
@@ -27,29 +27,26 @@ def search_text(character, path):
         if character in zip_read:
             index = zip_read.index(character)
             files_matched += 1
-            output = 'Match found in file ' + file + '\n' + zip_read[index - 40: index + 40]
+            output = 'Match found in file ' + file + '\n' + '...{}...'.format(zip_read[index - 40: index + 40])
             print(output)
 
     print('Total files searched: {}').format(files_searched)
     print('Total items matched: {}').format(files_matched)
 
 
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', help='add a directory')
-    parser.add_argument('character', help='search for a text')
+    parser.add_argument('character', help='search for a text item')
     args = parser.parse_args()
-
     character = args.character
     dir = args.dir
-
-    if os.path.isdir(dir):
+    if dir:
         search_text(character, dir)
     else:
         print('{} is not a directory').format(args.dir)
         print('Please provide a directory to be searched.')
+
 
 if __name__ == '__main__':
     main()
